7e3c26d8932745c8af36c937060c85e02f9cfbe2
makaili <makl@yusys.com.cn>
2019-07-02 17:51:29 +0800

    贵金属产品

---
+++ src/main/java/com/coding/sales/trans/PreciousMetal.java
@@ -0,0 +1,43 @@
+package com.coding.sales.trans;
+
+import java.math.BigDecimal;
+
+public class PreciousMetal {
+    private  String productName;//商品名称
+    private  String unit;//商品单位
+    private BigDecimal price;//商品价格
+    private  Double discount;//商品折扣
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+}
